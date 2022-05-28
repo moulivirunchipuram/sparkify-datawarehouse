4f46862f4e45e807166cb4e1111edd8409c33adb
moulivirunchipuram <vsmouli@gmail.com>
2022-05-28 13:20:22 -0700

    Update create_tables.py

--- create_tables.py
+++ create_tables.py
@@ -2,26 +2,34 @@ import configparser
 import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
 
-# drop all the tables to enable repeated testing of the application
-# loops through the list of tables defined in drop_table_queries
+
 def drop_tables(cur, conn):
-    for query in drop_table_queries:
+    """drop all the tables to enable repeated testing of the application
+       loops through then drop_table_queries list specified in sql_queries.py
+    """
+    for query in drop_table_queries:       
         cur.execute(query)
         conn.commit()
 
 # create tables looping through list of tables defined in create_table_queries
 def create_tables(cur, conn):
+    """The following code loops through the create_table_queries list
+       specified in sql_queries.py
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 # This is the driver code.  
-# Read configuration details from dwh.cfg file
+
 
 def main():
+    # Read configuration details from dwh.cfg file
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
-
+    """Use the properties loaded in the above config object
+       to connect to access the cluster and connect to the database 
+    """
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
 
