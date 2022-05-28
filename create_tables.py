71b475a98098b61a8171c54cc5478b705cffd0f6
moulivirunchipuram <vsmouli@gmail.com>
2022-05-28 12:06:46 -0700

    Update create_tables.py

--- create_tables.py
+++ create_tables.py
@@ -2,18 +2,21 @@ import configparser
 import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
 
-
+# drop all the tables to enable repeated testing of the application
+# loops through the list of tables defined in drop_table_queries
 def drop_tables(cur, conn):
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
-
+# create tables looping through list of tables defined in create_table_queries
 def create_tables(cur, conn):
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
+# This is the driver code.  
+# Read configuration details from dwh.cfg file
 
 def main():
     config = configparser.ConfigParser()
